15dfebe75e213987a941927ce5a6d26d49382c4d
Sergio Vargas Vicente <svargasvicente@gmail.com>
2026-04-12 22:58:16 +0200

    2

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -1,8 +1,97 @@
-﻿#include <glad/glad.h>  // ¡Importante! Va primero
+﻿#include <glad/glad.h>  ¡
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
-int main() {
+#include <fstream>
+#include <sstream>
+
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseSHader(const std::string& filePath)
+{
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::fstream stream(filePath);
+    std::string line;
+    ShaderType type = ShaderType::NONE;
+    std::stringstream ss[2];
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                //set mode to vertex
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                //set mode to fragment
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else 
+        {
+            ss[(int)type] << line << '\n';
+        }
+    }
+    return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length*sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile"<< (type==GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader: " << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    GLuint program = glCreateProgram();
+    GLuint vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+    GLuint fs = CompileShader(GL_FRAGMENT_SHADER,fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
+
+
+
+int main() 
+{
     if (!glfwInit()) return -1;
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL CMake", NULL, NULL);
@@ -19,32 +108,53 @@ int main() {
         return -1;
     }
 
+    float positions[] =
+    {
+        -0.5f, -0.5f,
+        0.0f , 0.5f,
+        0.5f , -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1,&buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float),positions,GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    //ShaderProgramSource
+    
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
+
+
+
     double lastTime = glfwGetTime();
     int nbFrames = 0;
-
     while (!glfwWindowShouldClose(window)) 
     {
-        // Calcular FPS
+        //#################
+        //       FPS
         double currentTime = glfwGetTime();
         nbFrames++;
         // Si ha pasado más de 1 segundo, calculamos y reiniciamos
         if (currentTime - lastTime >= 1.0) {
             double msPerFrame = 1000.0 / double(nbFrames);
-
             // Creamos el string del título
             // Nota: El título original era "OpenGL con CMake"
             std::string newTitle = "OpenGL CMake - FPS: " + std::to_string(nbFrames) +
                                    " (" + std::to_string(msPerFrame).substr(0, 4) + " ms)";
-
             // Cambiamos el título de la ventana
             glfwSetWindowTitle(window, newTitle.c_str());
-
-            nbFrames = 0;
-            lastTime += 1.0;
+            nbFrames = 0;lastTime += 1.0;
         }
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Un color azul grisáceo
+        //##################
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES,0,3);
+       
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
